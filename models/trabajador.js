31c4ff823fc49abea802c18aa2476da448f6577b
Nicaragua <santiago.lizondo@gmail.com>
2023-04-06 18:16:13 -0300

    arreglado a datatypes

--- models/trabajador.js
+++ models/trabajador.js
@@ -1,22 +1,27 @@
 const { DataTypes, Model } = require('sequelize');
-const { Sequelize } = require('sequelize');
 
 module.exports = (sequelize) => {
+
   class Trabajador extends Model {}
+
   Trabajador.init({
     nombre: {
-        type: Sequelize.STRING,
-        allowNull: false,
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     apellido: {
-        type: Sequelize.STRING,
-        allowNull: true,
+      type: DataTypes.STRING,
+      allowNull: true,
     },
     zona: {
-        type: Sequelize.STRING,
-        allowNull: false, 
+      type: DataTypes.STRING,
+      allowNull: false, 
     },
-    }, { sequelize, modelName: 'trabajador', tableName: 'trabajadores' });
- 
+  }, {
+    sequelize,
+    modelName: 'Trabajador',
+    tableName: 'trabajadores',
+  });
+
   return Trabajador;
 };
