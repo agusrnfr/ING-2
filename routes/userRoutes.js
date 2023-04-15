3f1f03f0e1f911a4458049790b098265bf33acc9
Nicaragua <santiago.lizondo@gmail.com>
2023-04-15 20:42:29 -0300

    primer middleware

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,6 +4,15 @@ const { validarLogin , mostrarLogin } = require('../controllers/loginController'
 const { mostrarRegister, registrar } = require('../controllers/registerController');
 const { mostrarTrabajadores } = require('../controllers/trabajadorController');
 const { mostrarIndex } = require('../controllers/indexController');
+const session = require('express-session');
+
+const comprobar_sesion = (req, res, next) => {
+    if (session.loggedin) {
+      next();
+    } else {
+      res.redirect('/login');
+    }
+};
 
 //invocamos express
 const app = require('express').Router()
@@ -15,7 +24,7 @@ app.get('/', mostrarIndex)
 app.get('/contactar/trabajador/:id', mostrarCamposContactoTrabajador)
 app.post('/contactar/trabajador/:id', contactar)
 
-app.get('/table', mostrarTablaUsers)
+app.get('/table',comprobar_sesion, mostrarTablaUsers)
 
 app.get('/login', mostrarLogin)
 app.post('/login', validarLogin)
