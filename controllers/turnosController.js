4d673850713373e6130930c56ce18ba83e6ff88f
Nicaragua <santiago.lizondo@gmail.com>
2023-04-30 20:10:54 -0300

    una coma de mas

--- controllers/turnosController.js
+++ controllers/turnosController.js
@@ -1,9 +1,10 @@
+const Turno = require('../db/models/turno');
+
 const mostrarTodosLosTurnos = async (req, res) => {
-    const Turno = require('../db/models/turno.js');
     //const User = require('../models/user.js')(sequelize)
     const data = await Turno.findAll({
         raw: true,
-        attributes: ['id', 'fecha','banda_horaria',, 'estado', 'UserId', 'MascotumId'],
+        attributes: ['id', 'fecha','banda_horaria', 'estado', 'UserId', 'MascotumId'],
       })
        // .then (turnos => User.findByPk(turnos.UserId))
         .catch(error => {
