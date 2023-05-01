1686c6e579cca13a8720e73712dab9d5e4501f4d
agusrnfr <agustina.rojas193292@alumnos.info.unlp.edu.ar>
2023-04-30 21:49:59 -0300

    LO LOGRE

--- controllers/turnosController.js
+++ controllers/turnosController.js
@@ -1,16 +1,33 @@
 const Turno = require('../db/models/turno');
+const Mascota = require('../db/models/mascota');
+const User = require('../db/models/user');
 
 const mostrarTodosLosTurnos = async (req, res) => {
-    //const User = require('../models/user.js')(sequelize)
-    const data = await Turno.findAll({
+    const turnos = await Turno.findAll({
         raw: true,
-        attributes: ['id', 'fecha','banda_horaria', 'estado', 'UserId', 'MascotumId'],
-      })
-       // .then (turnos => User.findByPk(turnos.UserId))
+        include: [
+            { model: Mascota, as: 'Mascotum', attributes: ['nombre'] },
+            { model: User, as: 'User', attributes: ['name'] }]
+    })
         .catch(error => {
             console.log(error);
+            res.status(500).json({ message: 'Error al devolver los resultados' });
         });
-    console.log(data);
+    const data = turnos.map(turno => {
+        return {
+            id: turno.id,
+            fecha: turno.fecha,
+            banda_horaria: turno.banda_horaria,
+            estado: turno.estado,
+            practica: turno.practica,
+            createdAt: turno.createdAt,
+            updatedAt: turno.updatedAt,
+            UserId: turno.UserId,
+            MascotumId: turno.MascotumId,
+            nombre: turno['Mascotum.nombre'],
+            user: turno['User.name']
+        };
+    });
     res.render('turnos_listado.ejs', { data });
 };
 
