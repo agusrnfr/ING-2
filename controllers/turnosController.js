77a8bb26e857007808a3048c7b2cd9a4576125fd
agusrnfr <agustina.rojas193292@alumnos.info.unlp.edu.ar>
2023-05-04 20:38:49 -0300

    Verifico edades en backend

--- controllers/turnosController.js
+++ controllers/turnosController.js
@@ -5,84 +5,128 @@ const session = require('express-session');
 const moment = require('moment');
 const { transporter } = require('../config/mailer');
 
-const verificaciones = (req, res, next) => {
+const obtenerIdyFechaDeMascota = (str) => {
+    const parts = str.split("-");
+    const front = parts[0];
+    const dateParts = parts[1].split("/");
+    const date = new Date(dateParts[2], dateParts[1] - 1, dateParts[0]);
+    return { id: front, fechaNacimiento: date };
+}
+
+const buscarMascotasCliente = async (id) => {
+    const mascotas = await Mascota.findAll({ raw: true, where: { UserId: id } });
+    return mascotas;
+}
+
+const verificaciones = async (req, res, next) => {
     let { fecha_turno, banda_horaria, practica, mascota } = req.body;
+    let { id, fechaNacimiento } = obtenerIdyFechaDeMascota(mascota);
 
-    if (!fecha_turno || !moment(fecha_turno).isValid()) { //Verifica que la fecha sea válida
-        return res.status(404).json('La fecha de turno no es válida');
-    }
-    
-    fecha_turno = moment(fecha_turno).startOf('day'); // Establece la fecha al inicio del día
-    let dia = fecha_turno.day();
-
-    if (!practica || !mascota || !banda_horaria) { // Verifica que no haya campos vacíos
-        return res.status(404).json('No se puede solicitar turno con campos vacíos');
-    }
-
-    if (dia === 6 && banda_horaria.toLowerCase() === 'tarde' || dia === 0) { // Verifica que no se solicite turno para los sábados o domingos
-        return res.status(404).json('No se puede solicitar turno para las tardes de los sábados o los domingos');
+    try {
+        const mascotasCliente = await buscarMascotasCliente(session.usuario.id);
+        const mascotaSeleccionada = mascotasCliente.find(m => m.id == id);
+        if (!mascotaSeleccionada) { // Verifica que la mascota exista y pertenezca al usuario
+            return res.status(400).json('La mascota seleccionada no existe o no pertenece al usuario logueado');
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json('Error al buscar las mascotas del usuario: ' + error);
     }
 
-    if (fecha_turno.isBefore(moment().startOf('day'))) { // Verifica que no se solicite turno para una fecha anterior a la actual
-        return res.status(404).json('No se puede solicitar turno para una fecha anterior a la actual');
-    }
+    if (practica == 'Castracion' || practica == 'Desparasitacion' || practica == 'Consulta general') { // Verifica que la fechea sea validad
 
-    if (fecha_turno.isAfter(moment().startOf('day').add(2, 'years'))) { // Verifica que no se solicite turno para una fecha muy lejana
-        return res.status(404).json('No se puede solicitar turno para una fecha muy lejana');
-    }
+        if (!fecha_turno || !moment(fecha_turno).isValid()) { //Verifica que la fecha sea válida
+            return res.status(400).json('La fecha de turno no es válida');
+        }
 
+        fecha_turno = moment(fecha_turno).startOf('day'); // Establece la fecha al inicio del día
+        let dia = fecha_turno.day();
+
+        if (!practica || !mascota || !banda_horaria) { // Verifica que no haya campos vacíos
+            return res.status(400).json('No se puede solicitar turno con campos vacíos');
+        }
+
+        if (dia === 6 && banda_horaria.toLowerCase() === 'tarde' || dia === 0) { // Verifica que no se solicite turno para los sábados o domingos
+            return res.status(404).json('No se puede solicitar turno para las tardes de los sábados o los domingos');
+        }
+
+        if (fecha_turno.isBefore(moment().startOf('day'))) { // Verifica que no se solicite turno para una fecha anterior a la actual
+            return res.status(400).json('No se puede solicitar turno para una fecha anterior a la actual');
+        }
+
+        if (fecha_turno.isAfter(moment().startOf('day').add(2, 'years'))) { // Verifica que no se solicite turno para una fecha muy lejana
+            return res.status(400).json('No se puede solicitar turno para una fecha muy lejana');
+        }
+    }
+    if (practica == 'Vacuna A') {
+        if (moment().startOf('day').diff(moment(fechaNacimiento).startOf('day'), 'months') < 2) { // Verifica que la mascota tenga al menos 2 meses
+            return res.status(400).json('La mascota debe tener al menos 2 meses para solicitar la vacuna A');
+        }
+    }
+    if (practica == 'Vacuna B') {
+        if (moment().startOf('day').diff(moment(fechaNacimiento).startOf('day'), 'months') < 4) { // Verifica que la mascota tenga al menos 4 meses
+            return res.status(400).json('La mascota debe tener al menos 4 meses para solicitar la vacuna B');
+        }
+    }
+    if (practica == 'Castracion') {
+        if (moment().startOf('day').diff(moment(fechaNacimiento).startOf('day'), 'months') < 6) { // Verifica que la mascota tenga al menos 6 meses
+            return res.status(400).json('La mascota debe tener al menos 6 meses para solicitar la castración');
+        }
+    }
     next(); //si pasa todas las verificaciones, pasa al siguiente middleware
 }
 
-
-
 const solicitarTurno = async (req, res) => {
-    const id = session.usuario.id;
-    const mascotas = await Mascota.findAll({ where: { UserId: id } })
-        .catch(error => {
-            console.log(error);
-            res.status(500).json('Error al devolver los resultados: ' + error);
-        });
-    res.render('solicitar_turno.ejs', { mascotas });
+    const UserId = session.usuario.id;
+    try {
+        const mascotas = await buscarMascotasCliente(UserId);
+        res.render('solicitar_turno.ejs', { mascotas });
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).json('Error al devolver los resultados: ' + error);
+    }
 };
 
 const guardarTurno = async (req, res) => {
     const { fecha_turno, banda_horaria, practica, mascota } = req.body;
     const UserId = session.usuario.id;
-    const mascotas = await Mascota.findAll({ where: { UserId: UserId } }) //    ESTO SE REPITE, DEBERIA ESTAR EN UNA FUNCION
-        .catch(error => {
-            console.log(error);
-            res.status(500).json('Error al devolver los resultados: ' + error);
-        });
-    await Turno.create({
-        fecha: moment(fecha_turno).toDate(),
-        banda_horaria: banda_horaria,
-        practica: practica,
-        UserId: UserId,
-        MascotumId: mascota,
-        estado: "Pendiente"
-    })
-        .catch(error => {
-            console.log(error);
-            res.status(500).render('solicitar_turno.ejs', {
-                mascotas,
-                alert: true,
-                alertTitle: "Error",
-                alertMessage: "Error al guardar el turno, por favor reingrese los datos",
-                alertIcon: "error",
-                showConfirmButton: true,
-                timer: false,
+    try {
+        const mascotas = await buscarMascotasCliente(UserId);
+        await Turno.create({
+            fecha: moment(fecha_turno).toDate(),
+            banda_horaria: banda_horaria,
+            practica: practica,
+            UserId: UserId,
+            MascotumId: mascota.id,
+            estado: "Pendiente"
+        })
+            .catch(error => {
+                console.log(error);
+                res.status(500).render('solicitar_turno.ejs', {
+                    mascotas,
+                    alert: true,
+                    alertTitle: "Error",
+                    alertMessage: "Error al guardar el turno, por favor reingrese los datos",
+                    alertIcon: "error",
+                    showConfirmButton: true,
+                    timer: false,
+                });
             });
+        res.render('solicitar_turno.ejs', {
+            mascotas,
+            alert: true,
+            alertTitle: "Solicitud exitosa",
+            alertMessage: "Su solicitud ha sido registrada, puede verificarla en su listado de turnos",
+            alertIcon: "success",
+            showConfirmButton: true,
+            timer: 1500,
         });
-    res.render('solicitar_turno.ejs', {
-        mascotas,
-        alert: true,
-        alertTitle: "Solicitud exitosa",
-        alertMessage: "Su solicitud ha sido registrada, puede verificarla en su listado de turnos",
-        alertIcon: "success",
-        showConfirmButton: true,
-        timer: 1500,
-    });
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).json('Error al devolver los resultados: ' + error);
+    }
 };
 
 
--- seeders/20230430130200-seed-mascotas.js
+++ seeders/20230430130200-seed-mascotas.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   /** @type {import('sequelize-cli').Migration} */
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     const moment = require('moment');
     const faker = require('faker');
     await queryInterface.bulkInsert('mascotas', [{
@@ -38,7 +38,7 @@ module.exports = {
       fecha_nacimiento: moment('2018-01-12').toDate(),
       observaciones: 'Ladra mucho y le gusta comer',
       foto: null,
-      UserId: 3,
+      UserId: 3, //JUANCHO
     }], {});
     await queryInterface.bulkInsert('mascotas', [{
       nombre: 'Bart',
@@ -47,22 +47,69 @@ module.exports = {
       fecha_nacimiento: moment('2018-01-12').toDate(),
       observaciones: 'Es muy cariñoso y le gusta jugar con la pelota',
       foto: null,
-      UserId: 5,
+      UserId: 5, //NAMI
+    }], {});
+    await queryInterface.bulkInsert('mascotas', [{
+      nombre: 'Chuchi',
+      raza: 'Salchicha',
+      color: 'Marron',
+      fecha_nacimiento: moment().clone().subtract(5, 'months').toDate(),
+      observaciones: 'Es muy cariñoso y le gusta jugar con la pelota',
+      foto: null,
+      UserId: 6, //AGUSTINA
+    }], {});
+    await queryInterface.bulkInsert('mascotas', [{
+      nombre: 'Vani',
+      raza: 'Pitbull',
+      color: 'Negro',
+      fecha_nacimiento: moment().clone().subtract(1, 'months').toDate(),
+      observaciones: 'Es muy cariñoso y le gusta jugar con la pelota',
+      foto: null,
+      UserId: 4, //AGUSTIN
+    }], {});
+    await queryInterface.bulkInsert('mascotas', [{
+      nombre: 'Rudo',
+      raza: 'Golden Retriever',
+      color: 'Negro',
+      fecha_nacimiento: moment().clone().subtract(3, 'months').toDate(),
+      observaciones: 'Es muy cariñoso y le gusta jugar con la pelota',
+      foto: null,
+      UserId: 3,
+    }], {});
+    await queryInterface.bulkInsert('mascotas', [{
+      nombre: 'Syndra',
+      raza: 'Cruza',
+      color: 'Negro',
+      fecha_nacimiento: moment().clone().subtract(6, 'months').toDate(),
+      observaciones: 'Es muy cariñoso y le gusta jugar con la pelota',
+      foto: null,
+      UserId: 6,
+    }], {});
+    await queryInterface.bulkInsert('mascotas', [{
+      nombre: 'Salem',
+      raza: 'Cruza',
+      color: 'Negro',
+      fecha_nacimiento: moment().clone().subtract(3, 'months').toDate(),
+      observaciones: 'Es muy cariñoso y le gusta jugar con la pelota',
+      foto: null,
+      UserId: 7,
     }], {});
     for (let i = 0; i < 10; i++) {
+      const today = new Date();
+      const minDate = new Date(today.getFullYear() - 18, today.getMonth(), today.getDate());
       await queryInterface.bulkInsert('mascotas', [{
         nombre: faker.name.firstName(),
         raza: faker.random.arrayElement(['Golden Retriever', 'Caniche', 'Labrador', 'Bulldog', 'Pitbull', 'Pastor Aleman', 'Cruza']),
         color: faker.random.arrayElement(['Negro', 'Blanco', 'Amarillo', 'Marron', 'Blanco y Negro', 'Amarillo y Marron', 'Gris', 'Gris y Blanco']),
-        fecha_nacimiento: faker.date.future(),
-        observaciones: faker.random.arrayElement(['Es muy jugueton', 'Es muy nervioso', 'Agresivo','Calmado']),
+        fecha_nacimiento: faker.date.between(minDate, today),
+        observaciones: faker.random.arrayElement(['Es muy jugueton', 'Es muy nervioso', 'Agresivo', 'Calmado']),
         foto: null,
         UserId: faker.datatype.number({ min: 3, max: 8 }),
       }], {});
     }
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     // Comandos para revertir la inserción de datos
   }
 };
