59de74290e5ae3bf1a468aef0c8a368884150c91
Nicaragua <santiago.lizondo@gmail.com>
2023-05-13 11:14:42 -0300

    Merge branch 'lau' into scrum-lunes-15-mayo

--- controllers/adopcionController.js
+++ controllers/adopcionController.js
@@ -1,53 +1,34 @@
 const { error } = require('jquery');
 const Adopcion = require('../db/models/adopcion.js');
+const User = require('../db/models/user');
 
 const session = require('express-session');
 
 const mostrarAdopciones = async (req, res) => {
-    try {
-      const data = await Adopcion.findAll()
-      const esDuenio = session.usuario && session.usuario.tipo === 'duenio';
+  try {
+     const data = await Adopcion.findAll();
+     let adopcionesOrdenadas = data.sort(compararPorEstado);
 
-      if(data.length === 0) {
-        res.send('No hay publicaciones de adopción disponibles.')
-      } else {
-        res.render('adopciones', { data , esDuenio , session: session , adopcionId: data[0].id})
-      }
-    } catch (error) {
-      console.error(error)
-      res.send('Hubo un error al cargar las publicaciones de adopción.')
+    if (adopcionesOrdenadas.length === 0) {
+      res.render('adopciones', { data: adopcionesOrdenadas, session: session });
+    } else {
+      res.render('adopciones', { data: adopcionesOrdenadas, session: session });
     }
-}
+  } catch (error) {
+    console.error(error);
+    res.send('Hubo un error al cargar las publicaciones de adopción.');
+  }
+};
 
-
-//Devuelve si es el dueño de la publicacion
-const esDuenio = async (req, res) => {
-  const adopcionId = req.params.id;
-  const usuarioId = session.usuario.id; 
-
-  console.log('adopcionId:', adopcionId);
-  console.log('usuarioId:', usuarioId);
-  
-  const publicacion = await Adopcion.findOne({
-    where: {
-      id: adopcionId,
-      userid: usuarioId
-    }
-  });
-
-  if (publicacion) {
-    res.send(true); // El usuario es el dueño de la publicación
+function compararPorEstado(a, b) {
+  if (a.se_adopto && !b.se_adopto) {
+    return 1;
+  } else if (!a.se_adopto && b.se_adopto) {
+    return -1;
   } else {
-    res.send(false); // El usuario no es el dueño de la publicación
+    return 0;
   }
 }
-
-  const resultado = async (req, res) => {
-   const adopcion = await Adopcion.findByPk(req.params.id);
-   const esDuenio = req.esDuenio; // Obtener el valor de req.esDuenio
-   res.render('adopciones', { adopcion, esDuenio }); // Enviar resultados a la plantilla
-  };
-
 
 const cambiarEstado = async (req, res) => {
   const adopcionId = req.body.id;
@@ -65,17 +46,20 @@ const cambiarEstado = async (req, res) => {
   });
   
   if (adopcion) {
-    // Actualizar el estado de adopción de la publicación
-    await Adopcion.update(
-      { se_adopto: !adopcion.se_adopto },
-      { where: { id: adopcionId } }
-    );
-
-    res.send(true);
-    console.log('Se cambio el estado')
+    try {
+      // Actualizar el estado de adopción de la publicación
+      await Adopcion.update(
+        { se_adopto: !adopcion.se_adopto },
+        { where: { id: adopcionId } }
+      );
+  
+      res.json({ success: true }); // Envía una respuesta JSON indicando que el cambio de estado se realizó correctamente
+    } catch (error) {
+      console.error(error);
+      res.json({ success: false }); // Envía una respuesta JSON indicando que hubo un error al cambiar el estado
+    }
   } else {
-    res.send(false);
-    console.log('No se cambio el estado :(')
+    res.json({ success: false }); // Envía una respuesta JSON indicando que el usuario no es el dueño de la publicación
   }
 }
 
@@ -88,6 +72,7 @@ const mostrarPublicacion = (req, res) => {
 const guardarPublicacion = async (req, res) => {    
   const nombre = req.body.nombre;
   const edad = req.body.edad;
+  const tipo_edad = req.body.tipo_edad;
   const raza = req.body.raza;
   const color = req.body.color;
   const sexo = req.body.sexo;
@@ -109,6 +94,7 @@ const guardarPublicacion = async (req, res) => {
       nombre:nombre,
       vacunas:vacunas,
       edad:edad,
+      tipo_edad: tipo_edad,
       sexo: sexo,
       origen:origen,
       mail:mail,
@@ -144,14 +130,65 @@ const guardarPublicacion = async (req, res) => {
   
 }
 
+//Contacto adoptantes
+const mostrarContacto = (req, res) => {
+  let nombre = '';
+  let mail = '';
+  let telefono = '';
+
+  if (session && session.usuario && session.loggedin) {
+    // Si el usuario ha iniciado sesión, pre-cargamos los datos del usuario
+    nombre = session.usuario.name;
+    mail = session.usuario.mail;
+    telefono = session.usuario.tel;
+  }
+
+  console.log(session);
+
+  res.render('contactoAdoptante', {
+    session: session,
+    nombre: nombre,
+    mail: mail,
+    telefono: telefono,
+  });
+};
+
+const contactoAdoptante = async (req, res) => {
+
+  const nombre = req.body.nombre;
+  const mail = req.body.mail;
+  const telefono = req.body.telefono;
+
+  if (!nombre || !mail || !telefono) {
+    console.error('Error al crear publicación, campos incompletos');
+    res.render('contactoAdoptante', {
+      alert: true,
+      alertTitle: 'Error al crear publicación',
+      alertMessage: 'Por favor, completa todos los campos requeridos',
+      alertIcon: 'error',
+      showConfirmButton: false,
+      timer: 2000,
+    });
+  } else {
+    // Aquí se realizaría el envío del correo electrónico
+    // y se mostraría la alerta de éxito en caso de que el envío haya sido exitoso.
+    res.render('contactoAdoptante', {
+      alert: true,
+      alertTitle: 'Mail Enviado!',
+      alertMessage: '',
+      alertIcon: 'success',
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  }
+};
 
 
 module.exports = {
     mostrarAdopciones,
-    esDuenio,
     cambiarEstado,
     mostrarPublicacion,
     guardarPublicacion,
-    resultado,
-  
+    mostrarContacto,
+    contactoAdoptante,
 }
