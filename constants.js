80f426c5a0019c3a41dc588d590cf7841846bfee
Nicaragua <santiago.lizondo@gmail.com>
2023-06-04 16:55:43 -0300

    password y razas

--- constants.js
+++ constants.js
@@ -1,3 +1,22 @@
 module.exports = {
     DESPARASITACION: 'Desparasitacion',
+    razas: {
+      CRUZA: 'Cruza',
+      BORDER_COLLIE: 'Border Collie',
+      GOLDEN_RETRIEVER: 'Golden Retriever',
+      LABRADOR_RETRIEVER: 'Labrador Retriever',
+      CANICHE: 'Caniche',
+      BULLDOG: 'Bulldog',
+      BULLDOG_FRANCES: 'Bulldog Frances',
+      BEAGLE: 'Beagle',
+      ROTTWEILER: 'Rottweiler',
+      PUG: 'Pug',
+      SALCHICHA: 'Salchicha',
+      CHIHUAHUA: 'Chihuahua',
+      BOXER: 'Boxer',
+      PASTOR_ALEMAN: 'Pastor Aleman',
+      PASTOR_BELGA: 'Pastor Belga',
+      PASTOR_AUSTRALIANO: 'Pastor Australiano',
+      PASTOR_INGLES: 'Pastor Ingles',
+    }
   };
